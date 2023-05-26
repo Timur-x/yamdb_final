4bc02dde825efadb0a468f33ed931c2a28d0ee12
Timur-x <niktim73431@yandex.ru>
2023-05-26 06:45:39 +0300

    restartdep

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.tokens import default_token_generator # noqa.
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models.signals import post_save # noqa.
+from django.dispatch import receiver # noqa.
 
 from .validators import validate_username, validate_year
 

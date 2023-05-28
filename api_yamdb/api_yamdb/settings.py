c0cfd76624e9d555ef51ed25e98e44baee1ceaf5
Timur-x <niktim73431@yandex.ru>
2023-05-28 19:01:28 +0300

    restart

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,12 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '158.160.55.101',
+    'web',
+    ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
